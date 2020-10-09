0df9617be18fe68feaf7ebd61d0ab52ccf09a8f4
Drakx116 <alexandre2.guyot@gmail.com>
2020-10-09 18:28:59 +0200

    Init range values from box size

--- week-2/sandbox/assets/js/sandbox.js
+++ week-2/sandbox/assets/js/sandbox.js
@@ -5,8 +5,11 @@ const box = document.getElementById('box');
 const widthSelect = document.getElementById('width-select');
 const heightSelect = document.getElementById('height-select');
 
-// SHOW INPUT TYPE RANGE VALUES
+// Initial inputs value
+widthSelect.value = getComputedStyle(box).width.replace('px', '');
+heightSelect.value = getComputedStyle(box).width.replace('px', '');
 
+// SHOW INPUT TYPE RANGE VALUES
 
 
 // EVENT LISTENERS
