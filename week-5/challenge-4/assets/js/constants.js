364f169b86ef3b483d7c8161bc3cce5c7532d046
Drakx116 <alexandre2.guyot@gmail.com>
2020-11-02 16:32:49 +0100

    Init dot colors constants

--- week-5/challenge-4/assets/js/constants.js
+++ week-5/challenge-4/assets/js/constants.js
@@ -6,3 +6,10 @@ const RADIUS = 15;
 // MISCELLANEOUS
 const MAX_IPS = 144;
 const LOOP = 2 * Math.PI;
+
+const COLOR = {
+  'DEFAULT': '#404E5C',
+  'DOUBLE': '#53A2BE',
+  'BONUS': '#0CCA4A',
+  'MALUS': '#D7263D',
+};
