848c364ec615097903fa00eb153b1ad563ee0b82
Drakx116 <alexandre2.guyot@gmail.com>
2020-11-02 16:33:50 +0100

    Generates new random dots

--- week-5/challenge-4/assets/js/seeder.js
+++ week-5/challenge-4/assets/js/seeder.js
@@ -1,10 +1,36 @@
 class Seeder {
   generateNewDot = () => {
+    const random = Math.floor(Math.random() * 10);
+
+    return this.generateRandomDot(random);
+  };
+
+  generateRandomDot(random) {
     const dot = new Dot();
 
-    dot.pos.x = Math.floor(Math.random() * (WIDTH - RADIUS - RADIUS) + RADIUS);
-    dot.pos.y = Math.floor(Math.random() * (HEIGHT - RADIUS - RADIUS) + RADIUS);
+    let radius = 14;
+    let color = COLOR.DEFAULT;
+
+    switch (random) {
+      case 7:
+        radius = 12;
+        color = COLOR.DOUBLE;
+        break;
+      case 8:
+        radius = 10;
+        color = COLOR.BONUS;
+        break;
+      case 9:
+        radius = 14;
+        color = COLOR.MALUS;
+        break;
+    }
+
+    dot.radius = radius;
+    dot.color = color;
+    dot.pos.x = Math.floor(Math.random() * (WIDTH - 2*radius) + radius);
+    dot.pos.y = Math.floor(Math.random() * (HEIGHT - 2*radius) + radius);
 
     return dot;
-  };
+  }
 }
