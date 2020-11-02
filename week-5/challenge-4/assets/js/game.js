13059beac4757dfcf3b01b687b01a911f1af1867
Drakx116 <alexandre2.guyot@gmail.com>
2020-11-02 15:43:17 +0100

    Add dot generation method

--- week-5/challenge-4/assets/js/game.js
+++ week-5/challenge-4/assets/js/game.js
@@ -1,23 +1,22 @@
 const canvas = document.getElementById('game').getContext('2d');
 const gui = new GUI(canvas);
 
-const dots = [];
+let dots = [];
 
-const animation = () => {
-  generateNewDot(dots);
-  gui.clear();
-  gui.drawDots(dots);
+const generateNewDot = () => {
+    const dot = new Dot();
+
+    dot.pos.x = Math.floor(Math.random() * (WIDTH - RADIUS - RADIUS) + RADIUS);
+    dot.pos.y = Math.floor(Math.random() * (HEIGHT - RADIUS - RADIUS) + RADIUS);
+
+    return dot;
+};
+
+const animation = (dots) => {
+  dots.push(generateNewDot());
+
+  gui.redraw(dots);
 }
 
-animation();
 
-const generateNewDot = (dots) => {
-  // Déclarer un nouveau point
-
-  // Affecter ses dimensios
-
-  // Ajouter le point au tableau dots
-
-  // BONUS : Position dans le Canavs
-  // constants.js : WIDTH 1000, HEIGHT 800
-};
+animation(dots);
