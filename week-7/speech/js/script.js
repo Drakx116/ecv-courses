5408911ddd23090a2983fcfc3855fba1ae4ab518
Drakx116 <alexandre2.guyot@gmail.com>
2020-11-13 15:30:28 +0100

    Init recognition

--- week-7/speech/js/script.js
+++ week-7/speech/js/script.js
@@ -1,8 +1,28 @@
 // Init
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
 const recognition = new window.SpeechRecognition();
 recognition.interimResults = true;
 
 // Global
 const words = document.getElementById('words');
-let sentence = document.createElement('li');
-words.appendChild(sentence);
+
+/// EVENTS
+
+recognition.addEventListener('result', event => {
+  let sentence = document.createElement('li');
+
+  sentence.textContent = Array.from(event.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  // End of the sentence
+  if (event.results[0].isFinal) {
+    words.appendChild(sentence);
+  }
+});
+
+recognition.start();
+recognition.addEventListener('end', recognition.start);
